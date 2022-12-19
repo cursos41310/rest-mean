efc4e1962ad1f77164303cb7cb4598de351847b0
dell5104 <cursos41310@gmail.com>
2022-12-19 17:29:25 +0100

    Swagger Docs

--- routes/auth.js
+++ routes/auth.js
@@ -8,11 +8,96 @@ const { registerUsuario, loginUsuario } = require("../controllers/authController
 
 const authRouter = Router();
 
+/**
+ * @swagger
+ * components: 
+ *  schemas:
+ *    Usuario:
+ *      type: object
+ *      properties:
+ *        username:
+ *          type: string
+ *          description: Este es el nombre de Usuario    
+ *        email:
+ *          type: string
+ *          description: Este es el correo 
+ *        password:
+ *          type: string
+ *          description: Esta es la contraseña, necesita minimo ser de 6 caracteres
+ *      required:
+ *        - username 
+ *        - email
+ *        - password
+ *      example:
+ *        username: JLBG
+ *        email: jlbg@mail.com
+ *        password: password123          
+ */
+
+/**
+ * @swagger
+ * /auth/register:
+ *  post:
+ *      summary: Registra un nuevo usuario
+ *      tags: [Usuario]
+ *      requestBody: 
+ *          required: true
+ *          content: 
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      $ref: '#/components/schemas/Usuario'
+ *      responses:
+ *          501:
+ *              description: Correo ya registrado
+ *          200:
+ *              description: Usuario credo correctamente y retornado con JWT (token)
+ *                    
+ */
+
+
 authRouter.post("/register", [check("email","Formato invalido").isEmail(),
 check("password", "La contraseña tiene que tener 6 caracteres como minimo").isLength({min:6}),
 check("usermane", "El nombre de usuario es requerido").not().isEmpty(),
 validationErrors], 
 registerUsuario);
+
+/**
+ * @swagger
+ * /auth/login:
+ *  post:
+ *      summary: Inicio de sesion de un nuevo usuario con un Token valido
+ *      tags: [Usuario]
+ *      requestBody: 
+ *          required: true
+ *          content: 
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          email:
+ *                              type: string
+ *                              description: Este es el correo 
+ *                          password:
+ *                              type: string
+ *                              description: Esta es la contraseña, necesita minimo ser de 6 cara
+ *                      required:
+ *                          - username 
+ *                          - email
+ *                          - password
+ *                      example:
+ *                          username: JLBG
+ *                          email: jlbg@mail.com
+ *                          password: password123
+ *      responses:
+ *          401:
+ *              description: Usuario o contraseña invalida
+ *          200:
+ *              description: Usuario logueado correctamente y retornado un JWT (token)
+ *                    
+ */
+
+
 authRouter.post("/login", [check("email","Formato invalido").isEmail(),
 check("password", "La contraseña tiene que tener 6 caracteres como minimo").isLength({min:6}),
 validationErrors],
--- routes/tareas.js
+++ routes/tareas.js
@@ -6,16 +6,159 @@ const validationErrors = require("../middlewares/validationErrors");
 
 const router = Router();
 
+/**
+ * @swagger
+ * components: 
+ *  schemas:
+ *    Tarea:
+ *      type: object
+ *      properties:
+ *        nombre:
+ *          type: string
+ *          description: Este es el nombre de la tarea    
+ *        creator:
+ *          type: Usuario
+ *          description: Este es el id del usuario 
+ *        createdAt:
+ *          type: date
+ *          description: Esta es la fecha de creacion
+ *      required:
+ *        - nombre
+ *        - creator
+ *        - createdAt
+ *      example:
+ *        nombre: Aplicacion con Laravel
+ *        creator: 62ff2c17c8f68597ccccf976          
+ */
+
+
+/**
+ * @swagger
+ * /task/read:
+ *  get:
+ *      summary: Lee todas las tareas
+ *      tags: [Tarea]
+ *      parameters:
+ *          - name: x-auth-token
+ *            in: header
+ *            description: JWT Token valido
+ *            required: true
+ *            type: string
+ *      requestBody: 
+ *          required: false
+ *      responses:
+ *          404:
+ *              description: El usuario no tiene tareas visibles
+ *          200:
+ *              description: Listado de tareas del Usuario logueado
+ *                    
+ */
+
+ router.get("/read", [verifyToken], readTarea);
+
+ /**
+ * @swagger
+ * /task/create:
+ *  post:
+ *      summary: Crea una nueva Tarea
+ *      tags: [Tarea]
+ *      parameters:
+ *          - name: x-auth-token
+ *            in: header
+ *            description: JWT Token valido
+ *            required: true
+ *            type: string
+ *      requestBody: 
+ *          required: true
+ *          content: 
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties: 
+ *                          nombre:
+ *                              type: string
+ *                              description: El nombre de la tarea
+ *      responses:
+ *          200:
+ *              description: La tarea se creo correctamente
+ *                    
+ */
+
 router.post("/create", [
 check("nombre", "Requerido").not().isEmpty(), 
 validationErrors,
 verifyToken], createTarea);
-router.get("/read", [verifyToken], readTarea);
+
+ /**
+ * @swagger
+ * /task/update/{id}:
+ *  put:
+ *      summary: Actualizar una Tarea
+ *      tags: [Tarea]
+ *      parameters:
+ *          - name: id
+ *            in: path
+ *            description: Id de la tarea por actualizar
+ *            required: true
+ *            type: string
+ *          - name: x-auth-token
+ *            in: header
+ *            description: JWT Token valido
+ *            required: true
+ *            type: string
+ *      requestBody: 
+ *          required: true
+ *          content: 
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties: 
+ *                          nombre:
+ *                              type: string
+ *                              description: El nombre de la tarea
+ *      responses:
+ *          200:
+ *              description: La tarea se actualizo correctamente
+ *          404:
+ *              description: La tarea se actualizo incorrectamente
+ *                    
+ */
+
+
+
 router.put("/update/:id", [
     check("nombre", "Requerido").not().isEmpty(),
     validationErrors,
     verifyToken], updateTarea);
-router.delete("/delete/:id", [verifyToken], deleteTarea);
+
+    /**
+ * @swagger
+ * /task/delete/{id}:
+ *  delete:
+ *      summary: Eliminar una Tarea
+ *      tags: [Tarea]
+ *      parameters:
+ *          - name: id
+ *            in: path
+ *            description: Id de la tarea por actualizar
+ *            required: true
+ *            type: string
+ *          - name: x-auth-token
+ *            in: header
+ *            description: JWT Token valido
+ *            required: true
+ *            type: string
+ *      requestBody: 
+ *          required: false
+ *      responses:
+ *          200:
+ *              description: La tarea se elimino correctamente
+ *          404:
+ *              description: La tarea se elimino incorrectamente
+ *                    
+ */
+
+    router.delete("/delete/:id", [verifyToken], deleteTarea);
 
 
 
